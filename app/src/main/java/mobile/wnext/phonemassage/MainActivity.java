0119c97dc95b39e8c52c29d8ec54a333f9192500
WeryNguyen <nnguyen@rgis.com.au>
2014-12-16 17:07:07 +1100

    Fist commit

---
+++ app/src/main/java/mobile/wnext/phonemassage/MainActivity.java
@@ -0,0 +1,79 @@
+package mobile.wnext.phonemassage;
+
+import android.os.Vibrator;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button btnStart, btnStop;
+    Vibrator vibrator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+        btnStart = (Button) findViewById(R.id.btnVibrate);
+        btnStop = (Button) findViewById(R.id.btnStop);
+
+        btnStart.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(!vibrator.hasVibrator()) {
+            Toast.makeText(this, "This phone does not have a vibrator", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnStart) {
+            startVibrating();
+        }
+        else if(view == btnStop) {
+            stopVibrating();
+        }
+    }
+
+    private void stopVibrating() {
+        vibrator.cancel();
+    }
+
+    private void startVibrating() {
+        vibrator.vibrate(3000);
+    }
+}
